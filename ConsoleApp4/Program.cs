c6146fd70d1b615026bdb0a70eeae4924a24f59e
krain <krain@DESKTOP-ATK9FNK>
2022-11-28 00:07:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i > 0;i-=7)
+            {
+                Console.WriteLine(i + "-7");
+            }
+        }
+    }
+}
